ef11a8f2bdaa290f1d0f33219d3c079873e1717c
Adam Ryan <adamryan1983@gmail.com>
2021-05-05 18:04:11 -0230

    Initial Commit

---
+++ Shared/Weather_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_UIApp.swift
+//  Shared
+//
+//  Created by Adam Ryan on 2021-05-05.
+//
+
+import SwiftUI
+
+@main
+struct Weather_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
